3e907f25804bd1e17e46b5e54aacaa9479e66968
msaad10 <saad.muhammad@tenpearls.com>
2022-01-08 14:56:03 +0500

    Added my project

---
+++ src/test/java/io/magazine/makhzaneilm/MakhzaneilmApplicationTests.java
@@ -0,0 +1,13 @@
+package io.magazine.makhzaneilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakhzaneilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
