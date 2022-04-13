bcb054bbedc9c045eecf0390d094469d1f8d2ac9
msaad10 <saad.muhammad@tenpearls.com>
2022-04-13 12:10:51 +0500

    Remove unused function

--- src/test/java/io/magazine/makhzaneilm/MakhzaneilmApplicationTests.java
+++ src/test/java/io/magazine/makhzaneilm/MakhzaneilmApplicationTests.java
@@ -6,9 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MakhzaneilmApplicationTests {
 
-	@Test
-	void contextLoads() {
-		System.out.println("Tests");
-	}
-
 }
